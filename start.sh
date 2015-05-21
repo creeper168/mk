0bd30771f4c6579662550e9732a788cff16dda95
zhang yong <zhangy@dtdream.com>
2015-05-21 15:09:38 +0800

    share for hangzhou

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-qemu-system-x86_64 -m 256M -kernel iso/linux -nographic  -net nic,vlan=0 -net tap,vlan=0 -hda cf.vmdk -initrd iso/initrd.img -append "console=ttyS0 rdinit=/sbin/init kgdboc=ttyS0"  
+DEBUG="quiet"
+if [ "$1" == "-d" ]; then
+DEBUG="debug"
+fi
+
+qemu-system-x86_64 -m 256M -kernel iso/linux -nographic  -net nic,vlan=0 -net tap,vlan=0 -hda cf.vmdk -initrd iso/initrd.img -append "console=ttyS0 rdinit=/sbin/init kgdboc=ttyS0 $DEBUG"  
