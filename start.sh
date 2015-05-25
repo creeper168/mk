6a0848726b4a35c25393a7c2955164c8554146b0
zhang yong <zhangy@dtdream.com>
2015-05-25 13:45:22 +0800

    start.sh support iso

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 DEBUG="quiet"
 QEMU_CMD="qemu-system-x86_64"
+TEST_ISO=0
 
 while test $# -gt 0; do
 case "$1" in
@@ -13,6 +14,10 @@ case "$1" in
 		QEMU_CMD="/usr/libexec/qemu-kvm"
 		shift
 	;;
+	-i|--iso)
+		TEST_ISO=1
+		shift
+	;;
 	*)
 		break
 	;;
@@ -21,9 +26,14 @@ done
 
 [ ! -f cf.qcow2 ] &&  qemu-img convert -f vmdk cf.vmdk  -O qcow2 cf.vmdk cf.qcow2 
 
+if [ ${TEST_ISO} != 1 ] ; then
 ${QEMU_CMD} -m 256M -kernel iso/linux -nographic  	\
 	-net nic,vlan=0 -net tap,vlan=0 		\
 	-hda cf.qcow2 -initrd iso/initrd.img		\
-	 -append "console=ttyS0 rdinit=/sbin/init kgdboc=ttyS0 ${DEBUG}"  
+	 -append "console=ttyS0 rdinit=/sbin/init kgdboc=ttyS0 ${DEBUG}  kgdbwait"  
+else
+
+${QEMU_CMD} -m 256M  -cdrom image.iso -net nic,vlan=0 -net tap,vlan=0 -hda cf.qcow2 -boot d -nographic
+fi
 
 rm -f cf.qcow2
